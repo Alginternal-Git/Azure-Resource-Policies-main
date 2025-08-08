62aa8f31e681be40789a54357e2afe560b508dfb
Alginternal-Git <notifications@algorims.com>
2025-08-08 17:56:47 +0530

    Delete Azure-Resource-Policies-Main-1 directory

--- Azure-Resource-Policies-Main-1/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-of-carbon"              
-    storage_account_name = "storage88998898"           
-    container_name       = "container5579"            
-    key                  = "terraform.tfstate"
-  }
-}
